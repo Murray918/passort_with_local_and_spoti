0bb2440169f4142b557fbbc19e4a9ac3de29a595
murray918 <darbyguitars@gmail.com>
2019-03-03 21:25:45 -0600

    creating a local branch for local auth

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const session = ('express-session')
-const passport = require('passport')
-
+const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const session = require("express-session")
+const passport = require("passport")
 
 const PORT = 8080
 
 const app = express()
 
-
-mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/auth_demo", { useNewUrlParser: true }, () => {
+    console.log('connected to mongodb on auth_demo');
+    
+})
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(session({
-    secret: 'keyboard cat',
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: true,
     saveUninitialized: true
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-app.use(morgan('comdined'))
+  })
+)
 
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(morgan("comdined"))
 
-app.get('/', (req, res, next) => {
-    console.log('server was hit by the client')
-    res.send('you have atttacccckkkkeeeeedddd')
+app.get("/", (req, res, next) => {
+  console.log("server was hit by the client")
+  res.send("you have atttacccckkkkeeeeedddd")
 })
-
-
 
 app.listen(() => {
-    console.log(`Your server is running on ${PORT}`)
+  console.log(`Your server is running on ${PORT}`)
 })
-
